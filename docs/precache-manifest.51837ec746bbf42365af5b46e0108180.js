e0674f65c31dddc39139352d545afcd807732ce7
Neal Roessler <groessler24@gmail.com>
2019-04-15 14:41:11 +0800

    Adding docs for github pages

---
+++ docs/precache-manifest.51837ec746bbf42365af5b46e0108180.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c780d80c83fe667e4d8d",
+    "url": "/static/js/main.d4f58a51.chunk.js"
+  },
+  {
+    "revision": "0b392ec0f475e686476b",
+    "url": "/static/js/2.df3eadf6.chunk.js"
+  },
+  {
+    "revision": "c780d80c83fe667e4d8d",
+    "url": "/static/css/main.ea4cc2c8.chunk.css"
+  },
+  {
+    "revision": "75dca19820fd28893ffdd195557eb475",
+    "url": "/index.html"
+  }
+];
